284c5ebb49dd604659265a55d5a6deeb0a0a7330
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2025-01-20 11:50:55 +0200

    Examples 2025-01-13

---
+++ RandomInteger.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+/* Generates a random integer between 0 and 100. */
+public class RandomInteger {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int value = r.nextInt(100) + 1;
+
+        System.out.println(value);
+    }
+}
